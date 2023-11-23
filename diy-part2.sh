394e6f1b8df66b6494aa8a9c2ec0a67a8dd1c9db
kevinCSDN <81788909+kevinCSDN@users.noreply.github.com>
2023-11-23 09:56:43 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -26,5 +26,5 @@ sed -i 's/192.168.1.1/10.10.10.1/g' package/base-files/files/bin/config_generate
 git clone https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
 git clone https://github.com/jerrykuku/luci-app-argon-config package/luci-app-argon-config
 #git clone https://github.com/messense/aliyundrive-webdav package/aliyundrive-webdav
-git clone -b luci https://github.com/xiaorouji/openwrt-passwall package/luci-app-passwall
+svn export https://github.com/xiaorouji/openwrt-passwall/trunk/luci-app-passwall package/luci-app-passwall
 git clone https://github.com/xiaorouji/openwrt-passwall-packages package/openwrt-passwall-packages
